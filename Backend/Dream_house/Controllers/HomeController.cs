9c503d3bdb1f3118292a92a31700bace441a3353
StefanTroian <49533523+StefanTroian@users.noreply.github.com>
2022-01-19 13:40:18 +0200

    Home controller + postman

---
+++ Backend/Dream_house/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dream_house.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public static List<Home> homes = new List<Home>
+        {
+            new Home {Id = 1, Name = "House1", Type="Apartament"},
+            new Home {Id = 2, Name = "House2", Type="Vila"}
+        };
+
+        // GET
+        [HttpGet("")]
+        public IEnumerable<Home> GetHomes()
+        {
+            return homes;
+        }
+
+        [HttpGet("byId")]
+        public Home GetHomesById(int id)
+        {
+            return homes.FirstOrDefault(home => home.Id.Equals(id));
+        }
+
+        [HttpGet("byId/{id}")]
+        public Home GetHomesByIdInEndpoint(int id)
+        {
+            return homes.FirstOrDefault(home => home.Id.Equals(id));
+        }
+
+        [HttpGet("filters/{name}/{type}")]
+        public Home GetHomesWithFilters(string name, string type)
+        {
+            return homes.FirstOrDefault(home => home.Name.Equals(name) && home.Type.Equals(type));
+        }
+
+        [HttpGet("fromHeader")]
+        public Home GetHomeByIdFromHeader([FromHeader] int id)
+        {
+            return homes.FirstOrDefault(home => home.Id.Equals(id));
+        }
+
+        [HttpGet("fromQuery")]
+        public Home GetHomeByIdFromQuery([FromQuery] int id)
+        {
+            return homes.FirstOrDefault(home => home.Id.Equals(id));
+        }
+
+
+
+        // POST
+        [HttpPost]
+        public IActionResult Add(Home home)
+        {
+            homes.Add(home);
+            return Ok(homes);
+        }
+
+        [HttpPost("fromBody")]
+        public IActionResult AddWithBody([FromBody] Home home)
+        {
+            homes.Add(home);
+            return Ok(homes);
+        }
+
+
+
+        // UPDATE
+        public IActionResult Update([FromBody] Home home)
+        {
+            var homeIndex = homes.FindIndex((Home _home) => _home.Id.Equals(home.Id));
+            homes[homeIndex] = home;
+
+            return Ok(homes);
+        }
+
+        //public async Task<IActionResult> UpdateAsync([FromBody] Home home)
+        //{
+        //    var homeIndex = homes.FindIndex((Home _home) => _home.Id.Equals(home.Id));
+        //    homes[homeIndex] = home;
+
+        //    return Ok(homes);
+        //}
+
+
+
+        // DELETE
+        [HttpDelete]
+        public IActionResult Delete(Home home)
+        {
+            homes.Remove(home);
+            return Ok(homes);
+        }
+
+    }
+}
---
+++ Backend/Dream_house/Models/Home.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dream_house.Controllers
+{
+    public class Home
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        //[Range(0,20)]
+        public string Type { get; set; }
+    }
+}
